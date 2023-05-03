194dd473c40f0f07489745f3fb2be3e1e4d82f2c
박성원 <sw2park@gridone.co.kr>
2023-05-03 18:51:33 +0900

    2.2

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Seo from "../components/Seo";
 export default function a() {
 
   const API_KEY = "10923b261ba94d897ac6b81148314a3f";
-  const [moive, setMoives] = useState([]);
+  const [movie, setMoives] = useState([]);
 
   //`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
 
@@ -12,19 +12,26 @@ export default function a() {
 
     (async ()=>{
       const data = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
-      console.log(data);
-      
+      setMoives(data.results);
     })();
     
 
   },[]);
 
+  console.log(movie)
 
 
   return (
     <div>
       <Seo title="home"></Seo>
       <h1>hello world</h1>
+      <hr/>
+      {!movie && <h4>loading...</h4>}
+      {movie?.map((e)=>(
+        <div key={e.id}>
+          <h2>{e.original_title}</h2>
+        </div>
+      ))}
     </div>
 
   )
